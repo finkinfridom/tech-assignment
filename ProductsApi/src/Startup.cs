e94f1058007e85356d95cc373b1486a56566d405
Alberto Maghini <alberto.maghini@deltatre.com>
2019-03-27 12:50:26 -0400

    reviewed Dockerfile

--- ProductsApi/src/Startup.cs
+++ ProductsApi/src/Startup.cs
@@ -28,7 +28,7 @@ namespace ProductsApi
         {
             services.AddScoped<ProductService>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
